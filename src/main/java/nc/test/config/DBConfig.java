1e702b85436988c0b7e3786ae4e41ea0c102a8ba
tapu0815 <tusya_0119>
2019-03-13 17:37:57 +0300

    db config class review

--- src/main/java/nc/test/config/DBConfig.java
+++ src/main/java/nc/test/config/DBConfig.java
@@ -6,27 +6,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
-
-/*
-* Конфигурация БД
-*/
+// database configuration
 
 @Configuration
 @EnableTransactionManagement
 @PropertySource("classpath:database.properties")
 public class DBConfig {
+
     @Autowired
     private Environment env;
+
     @Bean
     public NamedParameterJdbcTemplate jdbcTemplate() {
         return new NamedParameterJdbcTemplate(getDataSource());
     }
+
+    // read properties from \src\main\resources\database.properties file
     @Bean
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
