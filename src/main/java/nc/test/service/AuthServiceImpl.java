338f87348993c1049ae6fcfc3b4903a9d6693899
Modest <modestovmax97@gmail.com>
2019-03-28 13:27:17 +0300

    -init update structure

--- src/main/java/nc/test/service/AuthServiceImpl.java
+++ src/main/java/nc/test/service/AuthServiceImpl.java
@@ -1,8 +1,6 @@
 package nc.test.service;
 
-import nc.test.controller.AuthController;
 import nc.test.dao.interfaces.UserDao;
-import nc.test.security.SecurityCheck;
 import nc.test.service.interfaces.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -23,11 +21,7 @@ public class AuthServiceImpl implements AuthService {
     UserDao userDao;
 
     @Override
-    public String getRole(String basic ) {
-        if (SecurityCheck.checkBasicAuth(basic))
-            return userDao.getUserByLogin(SecurityContextHolder.getContext().getAuthentication().getName()).get().getRole();
-        else {
-            return "";
-        }
+    public String getRole(String basic) {
+        return userDao.getUserByLogin(SecurityContextHolder.getContext().getAuthentication().getName()).get().getRole();
     }
 }
